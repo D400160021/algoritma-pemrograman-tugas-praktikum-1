032071d350682736faba2eaf3c9c902a771a62a3
Dwi Aji Pamungkas <dwiajipamungkas10@gmail.com>
2016-12-07 08:31:42 +0700

    Create praktikum 8.c

---
+++ praktikum 8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main ()
+{
+    /* definisi variable lokal */
+    char hasilujian = 'A';
+    switch(hasilujian)
+    {
+    case 'A' :
+        printf("Sangat Baik!\n");
+        break;
+    case 'B' :
+    case 'C' :
+        printf("Baik\n");
+        break;
+    case 'D' :
+        printf("Cukup\n");
+        break;
+    case 'F' :
+        printf("Sebaiknya coba lagi\n");
+        break;
+    default :
+        printf("Gagal\n");
+    }
+    printf("Nilai anda adalah %c\n", hasilujian );
+    return 0;
+}
