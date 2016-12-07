a7f0013d09b8a82828c9a23ead71b037e17044c5
Dwi Aji Pamungkas <dwiajipamungkas10@gmail.com>
2016-12-07 08:39:18 +0700

    Create praktikum 12.c

---
+++ praktikum 12.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a =100;
+    printf("eksekusi perulangan while\n");
+    while (a >=1)
+    {
+        printf("nilai dari a : %d \n",a);
+        a--;
+    }
+    return 0;
+}
