26d36164527df305aa39f8a332bd5629c42670db
Dwi Aji Pamungkas <dwiajipamungkas10@gmail.com>
2016-12-07 08:37:54 +0700

    Create praktikum 11.c

---
+++ praktikum 11.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10;
+    printf("eksekusi perulangan do-while\n");
+    do
+    {
+        printf("nilai dari a : %d\n",a);
+        a++;
+    }   while ( a < 15);
+    return 0;
+}
