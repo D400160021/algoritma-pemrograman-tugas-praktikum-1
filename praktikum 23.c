743791d323bf6e52dcf50d55f5541dc0cd530b1b
Dwi Aji Pamungkas <dwiajipamungkas10@gmail.com>
2016-12-16 09:23:34 +0700

    Create praktikum 23.c

---
+++ praktikum 23.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main ()
+{
+    int n[5]={25,26,27,28,29,30};
+    int a;
+
+
+    {
+        printf("tulis elemen \n ");
+        scanf("%d",&n[a]);
+    }
+        printf("elemen ke-%d = %d\n",  n[a]);
+
+    return 0;
+}
